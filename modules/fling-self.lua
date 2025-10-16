5ea1f613056e7173699b9165a4e63ad246e66ad0
anosan <anospemula@gmail.com>
2025-10-17 05:29:18 +0800

    Update fling-self.lua

--- modules/fling-self.lua
+++ modules/fling-self.lua
@@ -1,12 +1,20 @@
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
-
 local Module = {}
+
 local bodyVelocity
+local characterAddedConnection
+local cleanupScheduled = false
 
 function Module.start()
+    Module.stop()
+    
     local character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
     local rootPart = character:WaitForChild("HumanoidRootPart")
+    
+    if bodyVelocity then
+        bodyVelocity:Destroy()
+    end
     
     bodyVelocity = Instance.new("BodyVelocity")
     bodyVelocity.Velocity = Vector3.new(
@@ -17,18 +25,35 @@ function Module.start()
     bodyVelocity.MaxForce = Vector3.new(9e9, 9e9, 9e9)
     bodyVelocity.Parent = rootPart
     
-    wait(0.5)
+    cleanupScheduled = true
+    task.delay(0.5, function()
+        if cleanupScheduled and bodyVelocity then
+            bodyVelocity:Destroy()
+            bodyVelocity = nil
+            cleanupScheduled = false
+        end
+    end)
+    
+    characterAddedConnection = LocalPlayer.CharacterAdded:Connect(function(newChar)
+        if bodyVelocity then
+            bodyVelocity:Destroy()
+            bodyVelocity = nil
+        end
+        cleanupScheduled = false
+    end)
+end
+
+function Module.stop()
+    cleanupScheduled = false
     
     if bodyVelocity then
         bodyVelocity:Destroy()
         bodyVelocity = nil
     end
-end
-
-function Module.stop()
-    if bodyVelocity then
-        bodyVelocity:Destroy()
-        bodyVelocity = nil
+    
+    if characterAddedConnection then
+        characterAddedConnection:Disconnect()
+        characterAddedConnection = nil
     end
 end
 
